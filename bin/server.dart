fefd7385a2016e17e5cfa62e87d16696bd3b8453
JTKnox91 <jtknox91@gmail.com>
2022-02-13 15:07:43 -0600

    Initial Server Setup

---
+++ bin/server.dart
@@ -0,0 +1,32 @@
+// Copyright (c) 2021, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+const htmlHeaders = <String, String>{
+  'Content-type': 'text/html',
+};
+
+Future main() async {
+  // If the "PORT" environment variable is set, listen to it. Otherwise, 8080.
+  // https://cloud.google.com/run/docs/reference/container-contract#port
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+
+  // See https://pub.dev/documentation/shelf/latest/shelf_io/serve.html
+  final server = await shelf_io.serve(
+    // See https://pub.dev/documentation/shelf/latest/shelf/logRequests.html
+    logRequests()
+        // See https://pub.dev/documentation/shelf/latest/shelf/MiddlewareExtensions/addHandler.html
+        .addHandler(_helloWorldHandler),
+    InternetAddress.anyIPv4, // Allows external connections
+    port,
+  );
+
+  print('Serving at http://${server.address.host}:${server.port}');
+}
+
+Response _helloWorldHandler(Request request) => Response.ok('Hello, World!', headers: htmlHeaders);
